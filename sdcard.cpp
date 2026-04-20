cc86bab73d9508234d50946287bb40c58a0e689a
gronktonkbabonk <gronktonkbabonk@proton.me>
2026-04-20 16:53:39 +0100

    omalawd

--- sdcard.cpp
+++ sdcard.cpp
@@ -21,6 +21,7 @@ SDCard::SDCard(spi_inst_t *spiInp, int csInp){
 }
 
 void SDCard::sdCardInit(){
+    sleep_ms(1);
     spi_init(spi, 1000*400); // clock rate to 400khz for init  
     FFClock(10);
 
@@ -42,6 +43,7 @@ void SDCard::sdCardInit(){
     else if(cmd8res == R1_ILLEGAL_COMMAND) v1Init();
     else err("Bad response from SD, unable to determine version.");
 
+    cmd(59, 1, 0x67);
     spi_init(spi,1000*1000*25);
 }
 
@@ -53,7 +55,7 @@ void SDCard::v2Init(){
     uint8_t r1;
     do
     {
-        cmd(55,0,0x65, 0, false);  
+        cmd(55,0,0x65, 0, response, false);  
         r1=cmd(41,0x40000000, 0, false);
     } while (r1 != 0x00 && timeout--);
     if (timeout==0) err("Failed to initialise v2 card, timed out while waiting.");
@@ -62,8 +64,9 @@ void SDCard::v2Init(){
     uint32_t ocr = (response[0] << 24)|(response[1] << 16)|(response[2] << 8)|response[3]; //reconstructing ocr
     if (!(ocr & 0x00FF8000)) err("v2 OCR Voltage out of range, Unusable card.");
     
-    addressMult = (((ocr >> 30) & 1)? 1 : 512);
+    addrMult = (((ocr >> 30) & 1)? 1 : 512);
     gpio_put(cs,1);
+    FFClock();
 }
 
 void SDCard::v1Init(){
@@ -71,7 +74,7 @@ void SDCard::v1Init(){
     uint8_t r1;
     do
     {
-        cmd(55,0,0x65, 0, false);  
+        cmd(55,0,0x65, 0, response, false);  
         r1=cmd(41,0, 0, false);
     } while ((r1 != 0x00) && timeout--);
     if (timeout==0) err("Failed to initialise v1 card, timed out while waiting.");
@@ -81,13 +84,13 @@ void SDCard::v1Init(){
     if (!(ocr & ((1 << 20) | (1 << 21)))) err("12 OCR Voltage out of range, Unusable card.");
 
     cmd(16, 512, 0);
-    addressMult = 512;
+    addrMult = 512;
     gpio_put(cs,1);
     
 }
 
 
-int SDCard::cmd(uint8_t cmd, uint32_t args, uint8_t crc, int extraResponseBytes, bool release, bool skip1){
+int SDCard::cmd(uint8_t cmd, uint32_t args, uint8_t crc, int extraResponseBytes, uint8_t responseBytesArray[], bool release, bool skip1){
     gpio_put(cs,0);
     FFClock();
     uint8_t buf[6] = {
@@ -109,11 +112,20 @@ int SDCard::cmd(uint8_t cmd, uint32_t args, uint8_t crc, int extraResponseBytes,
     memset(response, 0x00, 16);
     for (size_t i = 0; i < CMD_TIMEOUT; i++)
     {
-        spi_read_blocking(spi, 0xFF, &r1, 1);
+        spi_read_blocking(spi, FF_TOKEN, &r1, 1);
         if(!(r1&0x80)){ // checks the most significant bit of r1 is 1, which would indicate an error
-            for (size_t j = 0; j < extraResponseBytes; j++)
-            {
-                spi_read_blocking(spi, 0xFF, &response[j], 1);
+            if(extraResponseBytes != 0){
+                //read until start byte ()
+                int cmdTimeout = CMD_TIMEOUT;
+                do{
+                    uint8_t r1;
+                    spi_read_blocking(spi, FF_TOKEN, &r1, 1);
+                    sleep_ms(1);
+                } while(response != &FF_TOKEN && cmdTimeout > 0);
+                if(cmdTimeout == 0) err("timeout waiting for response");
+
+                spi_read_blocking(spi, FF_TOKEN, responseBytesArray, extraResponseBytes);
+                FFClock(2);
             }
             
             if(release){
@@ -128,6 +140,16 @@ int SDCard::cmd(uint8_t cmd, uint32_t args, uint8_t crc, int extraResponseBytes,
     gpio_put(cs,1);
     FFClock();
     return -1;
+}
+
+int SDCard::readBlocks(uint32_t blockAddr, size_t readNum, uint8_t buf[]){
+    blockAddr *= addrMult;
+    readNum *= addrMult;
+    int readCmd = (readNum == 1)? 17:18;
+    if(cmd(readCmd,blockAddr,0, readNum, buf,false) != 0) err("I/O error for read cmd");
+    uint8_t dummybuf[0];
+    if(readNum != 0) if(cmd(12,0,FF_TOKEN, 0,dummybuf,true) == 0x01) err("I/O error for multiread cmd.");
+    return 1;
 }
 
 int SDCard::FFClock(int clocks){
--- sdcard.h
+++ sdcard.h
@@ -5,15 +5,17 @@ class SDCard
 {
 private:
     int FFClock(int clocks=1);
-    uint8_t response[16];
+    uint8_t dummybuf[0];
+    uint8_t response[4];
     void err(const char* errMessage);
-    int addressMult;
+    int addrMult;
 public:
     spi_inst_t *spi;
     int cs;
     SDCard(spi_inst_t *spiInp, int csInp);
     void sdCardInit();
-    int cmd(uint8_t cmd, uint32_t args, uint8_t crc, int extraResponseBits=0, bool release = true, bool skip1 = true);
+    int cmd(uint8_t cmd, uint32_t args, uint8_t crc, int extraResponseBytes =0, const uint8_t responseBytesArray[]={}, bool release = true, bool skip1 = true);
     void v1Init();
     void v2Init();
+    int readBlocks(uint32_t blockAddr, size_t readNum, uint8_t buf[]);
 };
